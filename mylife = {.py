a0a5dcdc44483621abee45a6fba340a30f72d074
Javin13 <reachjavin@gmail.com>
2024-11-16 13:05:38 +0000

    Add files via upload

---
+++ mylife = {.py
@@ -0,0 +1,12 @@
+mylife = {
+    "name" : "JAvin",
+    "sports" : "tennis",
+    "religion" : "hindu",
+    "country" : "British Isles",
+    "nationality" : "British",
+    "car" : "BMW and MINI Cooper",
+    "food" : "noodles and chocolate",
+    "ice cream " : "vanilla",
+    "school" : "Oakwood Park Grammar School",
+}
+print(mylife)
